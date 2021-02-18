4b78b958a6ed815ccf7be466d9035838c648cc79
Abrellr <69308389+Abrellr@users.noreply.github.com>
2021-02-18 22:42:16 +0100

    edit copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { MDBContainer, MDBRow, MDBIcon } from 'mdbreact'
-import '../styles/footerStyle.css'
+import React from "react";
+import { MDBContainer, MDBRow, MDBIcon } from "mdbreact";
+import "../styles/footerStyle.css";
 
 export default function Footer() {
-    return (
-        <MDBContainer fluid id="footerContainer">
-            <MDBRow id="socialRow">
-                <a id="socialIcon" href="https://www.xing.com/profile/Rosmalina_Abrell"><MDBIcon id="socialIcon" fab icon="xing-square" size="2x" ></MDBIcon></a>
-                <a id="socialIcon" href="https://github.com/Abrellr"><MDBIcon id="socialIcon" fab icon="github" size="2x" ></MDBIcon></a>
-                <a id="socialIcon" href="https://linkedin.com/in/rosmalina-abrell"><MDBIcon id="socialIcon" fab icon="linkedin" size="2x" ></MDBIcon></a>
-            </MDBRow>
-            <MDBRow id="closingRow">
-            <p>Joyfully built on a react application</p>
-            </MDBRow>
-            <MDBRow id="copyRow">
-            <p>© Copyright 2020</p>
-            </MDBRow> 
-        </MDBContainer>
-    )
+  return (
+    <MDBContainer fluid id="footerContainer">
+      <MDBRow id="socialRow">
+        <a id="socialIcon" href="https://www.xing.com/profile/Rosmalina_Abrell">
+          <MDBIcon id="socialIcon" fab icon="xing-square" size="2x"></MDBIcon>
+        </a>
+        <a id="socialIcon" href="https://github.com/Abrellr">
+          <MDBIcon id="socialIcon" fab icon="github" size="2x"></MDBIcon>
+        </a>
+        <a id="socialIcon" href="https://linkedin.com/in/rosmalina-abrell">
+          <MDBIcon id="socialIcon" fab icon="linkedin" size="2x"></MDBIcon>
+        </a>
+      </MDBRow>
+      <MDBRow id="closingRow">
+        <p>Joyfully built on a react application</p>
+      </MDBRow>
+      <MDBRow id="copyRow">
+        <p>© Copyright 2021</p>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
